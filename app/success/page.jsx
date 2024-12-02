54c37c3065bd9dd2bcd0700663cfd5ec2fceeaf6
shawtester <96066025+shawtester@users.noreply.github.com>
2024-12-02 13:57:53 +0530

    first commit

--- app/success/page.jsx
+++ app/success/page.jsx
@@ -1,31 +1,9 @@
-"use client";
+import React from 'react'
 
-import React from "react";
-import { useSearchParams } from "next/navigation";
-
-export const dynamic = "force-dynamic";
-
-const SuccessPage = () => {
-  const searchParams = useSearchParams();
-
-  // Collect all query parameters into an object
-  const params = Array.from(searchParams.entries()).reduce((acc, [key, value]) => {
-    acc[key] = value;
-    return acc;
-  }, {});
-
+const page = () => {
   return (
-    <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
-      <h1 style={{ color: "green" }}>Payment Success</h1>
-      <div style={{ marginTop: "20px" }}>
-        {Object.keys(params).map((key) => (
-          <p key={key}>
-            <strong>{key}:</strong> {params[key]}
-          </p>
-        ))}
-      </div>
-    </div>
-  );
-};
+    <div>page</div>
+  )
+}
 
-export default SuccessPage;
+export default page
